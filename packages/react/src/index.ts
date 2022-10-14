9926ae197bb544537fb2a75bef1640a3682772b5
Gabriel Magreti <gabrielmagreti@gmail.com>
2022-10-13 21:51:48 -0300

    chore: add react and storybook and stitches

--- packages/react/src/index.ts
+++
@@ -1,3 +0,0 @@
-import { colors } from '@magreti-ui/tokens'
-
-console.log('colors', colors)
---
+++ packages/react/src/index.tsx
@@ -0,0 +1,9 @@
+import { colors } from '@magreti-ui/tokens'
+
+export function App() {
+  return (
+    <div>
+      <h1 style={{ color: colors.ignite300 }}>Welcome to react!</h1>
+    </div>
+  )
+}
