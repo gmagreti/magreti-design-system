a57c93059121c4102ebda513a149ebe9ca0c6a75
Gabriel Magreti <gabrielmagreti@gmail.com>
2022-11-16 13:41:36 -0300

    feat(): finalizing component Avatar

--- packages/react/src/Components/Avatar/index.tsx
+++ packages/react/src/Components/Avatar/index.tsx
@@ -1,5 +1,17 @@
-import { AvatarContainer } from './styles'
+import { User } from 'phosphor-react'
+import { ComponentProps } from 'react'
+import { AvatarContainer, AvatarFallback, AvatarImage } from './styles'
 
-export function Avatart() {
-  return <AvatarContainer />
+export interface AvatarProps extends ComponentProps<typeof AvatarImage> {}
+
+export function Avatar(props: AvatarProps) {
+  return (
+    <AvatarContainer>
+      <AvatarImage {...props} />
+
+      <AvatarFallback delayMs={600}>
+        <User />
+      </AvatarFallback>
+    </AvatarContainer>
+  )
 }
