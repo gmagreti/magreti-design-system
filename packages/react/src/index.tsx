00a3ec578aea50145c31effaf10946533400ad10
Gabriel Magreti <gabrielmagreti@gmail.com>
2022-11-16 12:39:57 -0300

    feat: exporting everything from component box

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,9 +1,1 @@
-import { colors } from '@magreti-ui/tokens'
-
-export function App() {
-  return (
-    <div>
-      <h1 style={{ color: colors.ignite300 }}>Welcome to react!</h1>
-    </div>
-  )
-}
+export * from './Components/Box'
